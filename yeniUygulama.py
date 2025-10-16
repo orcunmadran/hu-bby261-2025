de29a57574fbbae619d83ca6c1e397366107b210
Orçun Madran <orcunmadran@gmail.com>
2025-10-16 16:07:39 +0300

    Add files via upload

--- yeniUygulama.py
+++ yeniUygulama.py
@@ -1,5 +1,11 @@
 import rommenu
 
+def fonksiyon1():
+    print("Fonksiyon 1 çalıştırıldı.")
+
+def fonksiyon2():
+    print("Fonksiyon 2 çalıştırıldı.")
+
 uygmenu = rommenu.MenuSistemi
 uygmenu.karsilama("Yeni Uygulama")
-uygmenu.menuOlustur(["1. Başla", "2. Ayarlar", "3. Çıkış"])
+uygmenu.menuOlustur(["1. Fonksiyon A", "2. Fonksiyon B"])
